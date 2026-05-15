f15be2924508df8bf83a5f19a740b6770ad85169
Rossignol Hugo <hugo.rossignol1306@gmail.com>
2026-05-15 19:13:15 +0200

    Add files via upload

---
+++ footer/footer.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <div class="footer-inner">
+        <div class="footer-logo">Omnes<em>Event</em></div>
+        <div class="footer-lien">
+            <a href="">Accueil</a>
+            <a href="">Calendrier</a>
+            <a href="">Connexion</a>
+        </div>
+        <div class="footer-copy">
+            &copy; <?= date('Y') ?> OmnesEvent — Projet Web Dynamique ING2
+        </div>
+    </div>
+</footer>
