38c107fb290635ae31fd3077a540ee2c91b5f0ba
Rossignol Hugo <hugo.rossignol1306@gmail.com>
2026-05-15 15:54:55 +0200

    Add files via upload

---
+++ header/header.php
@@ -0,0 +1,22 @@
+<header>
+    <nav class="bar">
+        <div class="bar-int">
+            <a class="bar-logo" href="index.php">Omnes<em>Event</em></a>
+
+            <div class="bar-lien">
+                <a href="" class="bar-lien">🏠 Accueil</a>
+                <a href="" class="bar-lien">📅 Calendrier</a>
+                <a href="billet" class="bar-lien">🎫 Mes billets</a>
+                <a href="profil" class="bar-lien">👤 Mon profil</a>       
+                <a href="creerEvenement" class="bar-lien">✚ Créer un event</a>             
+                <a href="admin" class="bar-lien">⚙️ Admin</a>
+            </div>
+
+            <div class="Navigation-autentification">
+                <a href="deconnexion.php" class="btn-2">Déconnexion</a>
+                <a href="connexion.php" class="btn-1">Se connecter</a>
+                <a href="inscription.php" class="btn-2">S'inscrire</a>
+            </div>
+        </div>
+    </nav>
+</header>
