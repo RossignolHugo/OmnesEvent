98f981ecebe7d8152f374072574d47d3b313133f
Rossignol Hugo <hugo.rossignol1306@gmail.com>
2026-05-17 12:51:29 +0200

    footer 100%

---
+++ footer/footer.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <div class="footer-inner">
+        <div class="footer-logo">Omnes<em>Event</em></div>
+        <div class="footer-lien">
+            <a href="../index/index.php">Accueil</a>
+            <a href="../calendrier/calendrier.php">Calendrier</a>
+            <a href="../connexion/connexion.php">Connexion</a>
+        </div>
+        <div class="footer-copy">
+            &copy; <?= date('Y') ?> OmnesEvent — Projet Web Dynamique ING2
+        </div>
+    </div>
+</footer>
